3b687e9bb059b0dc5fe4cdbd4a16e567fd899534
Kasper Lyngsøe Gimbel <klg@it-minds.dk>
2020-01-24 16:23:27 +0100

    removed navbar tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RouterOutlet } from '@angular/router';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        RouterOutlet
       ],
     }).compileComponents();
   }));
